ef951f42352d6bb6e65d9a847e2099758145a4be
0xneves <asguilift@hotmail.com>
2024-05-30 06:53:18 +0700

    fixed: types

--- contracts/EXPCurves.sol
+++ contracts/EXPCurves.sol
@@ -2,11 +2,44 @@
 pragma solidity ^0.8.20;
 
 import {exp, div} from "@prb/math/src/ud60x18/Math.sol";
-import {wrap, unwrap} from "@prb/math/src/ud60x18/Casting.sol";
+import {wrap, unwrap, UD60x18} from "@prb/math/src/ud60x18/Casting.sol";
 
 // ((EXP(curvature * (timeDelta / totalTimeInterval)) - 1 ) / (EXP(curvature) - 1)) * 100
 // ((EXP(curvature * ( 1 - (timeDelta / totalTimeInterval))) - 1 ) / (EXP(curvature) - 1)) * 100
 contract ExponentialCurve {
+  function exponentialPercentage(
+    uint256 currentTimeframe,
+    uint256 initialTimeframe,
+    uint256 finalTimeframe,
+    uint256 curvature,
+    bool ascending
+  ) public pure returns (uint256) {
+    if (initialTimeframe > currentTimeframe) revert("underflow");
+    if (initialTimeframe > finalTimeframe) revert("underflow");
+
+    UD60x18 prc = wrap(1e18);
+    UD60x18 crv = wrap(curvature);
+
+    UD60x18 td;
+    UD60x18 tti;
+
+    td = wrap(currentTimeframe - initialTimeframe);
+    tti = wrap(finalTimeframe - initialTimeframe);
+
+    uint256 ter = unwrap(td / tti);
+    UD60x18 cs;
+    if (ascending) {
+      cs = wrap(curvature * ter);
+    } else {
+      cs = wrap(curvature * (1e18 - ter));
+    }
+
+    uint256 expo = unwrap(exp(cs)) - 1e18;
+    uint256 fes = unwrap(exp(wrap(curvature * 1e18))) - 1e18;
+
+    return unwrap(wrap(expo) / wrap(fes)) * 100;
+  }
+
   function curveNormalization(
     uint256 currentTimeframe,
     uint256 initialTimeframe,
@@ -28,7 +61,7 @@ contract ExponentialCurve {
           ),
           wrap(finalExpScaling(curvature))
         )
-      ) * 100;
+      ) * 100; // percentage adjustment
   }
 
   function finalExpScaling(uint256 curvature) public pure returns (uint256) {
@@ -79,21 +112,21 @@ contract ExponentialCurve {
 
   function timeElapsedRatio(
     uint256 currentTimeframe,
-    uint256 finalTimeframe,
-    uint256 initialTimeframe
+    uint256 initialTimeframe,
+    uint256 finalTimeframe
   ) public pure returns (uint256) {
     return
       unwrap(
         div(
           wrap(timeDelta(currentTimeframe, initialTimeframe)),
-          wrap(totalTimeInterval(finalTimeframe, initialTimeframe))
+          wrap(totalTimeInterval(initialTimeframe, finalTimeframe))
         )
       );
   }
 
   function totalTimeInterval(
-    uint256 finalTimeframe,
-    uint256 initialTimeframe
+    uint256 initialTimeframe,
+    uint256 finalTimeframe
   ) public pure returns (uint256) {
     unchecked {
       if (initialTimeframe > finalTimeframe) revert("underflow");
--- test/EXPCurves.test.ts
+++ test/EXPCurves.test.ts
@@ -24,10 +24,36 @@ describe("Exponential Curve", async function () {
 
   it("Should go from 100% to 0%", async function () {
     for (let i = 0; i < inputs.length; i++) {
+      const ep = await expCurves.exponentialPercentage(
+        inputs[i],
+        initialTimeframe,
+        finalTimeframe,
+        curvature,
+        false,
+      );
+      console.log(ep == 0 ? 0 : Number(ep) / 1e18);
+    }
+  });
+
+  it("Should go from 0 to 100%", async function () {
+    for (let i = 0; i < inputs.length; i++) {
+      const ep = await expCurves.exponentialPercentage(
+        inputs[i],
+        initialTimeframe,
+        finalTimeframe,
+        curvature,
+        true,
+      );
+      console.log(ep == 0 ? 0 : Number(ep) / 1e18);
+    }
+  });
+
+  it("Should go from 100% to 0%", async function () {
+    for (let i = 0; i < inputs.length; i++) {
       const cn = await expCurves.curveNormalization(
         inputs[i],
-        finalTimeframe,
         initialTimeframe,
+        finalTimeframe,
         curvature,
         false,
       );
@@ -39,8 +65,8 @@ describe("Exponential Curve", async function () {
     for (let i = 0; i < inputs.length; i++) {
       const cn = await expCurves.curveNormalization(
         inputs[i],
-        finalTimeframe,
         initialTimeframe,
+        finalTimeframe,
         curvature,
         true,
       );
@@ -51,24 +77,24 @@ describe("Exponential Curve", async function () {
   it("Should get Curve Normalization", async function () {
     const cn0 = await expCurves.curveNormalization(
       inputs[0],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
       curvature,
       true,
     );
     console.log(cn0.toString());
     const cn1 = await expCurves.curveNormalization(
       inputs[1],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
       curvature,
       true,
     );
     console.log(cn1.toString());
     const cn2 = await expCurves.curveNormalization(
       inputs[2],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
       curvature,
       true,
     );
@@ -83,24 +109,24 @@ describe("Exponential Curve", async function () {
   it("Should get Exponential (Euler)", async function () {
     const exp0 = await expCurves.exponential(
       inputs[0],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
       curvature,
       true,
     );
     console.log(exp0.toString());
     const exp1 = await expCurves.exponential(
       inputs[1],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
       curvature,
       true,
     );
     console.log(exp1.toString());
     const exp2 = await expCurves.exponential(
       inputs[2],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
       curvature,
       true,
     );
@@ -110,24 +136,24 @@ describe("Exponential Curve", async function () {
   it("Should get Curve Scalling Descending", async function () {
     const cs0 = await expCurves.curveScaling(
       inputs[0],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
       curvature,
       false,
     );
     console.log(cs0.toString());
     const cs1 = await expCurves.curveScaling(
       inputs[1],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
       curvature,
       false,
     );
     console.log(cs1.toString());
     const cs2 = await expCurves.curveScaling(
       inputs[2],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
       curvature,
       false,
     );
@@ -137,24 +163,24 @@ describe("Exponential Curve", async function () {
   it("Should get Curve Scalling Ascending", async function () {
     const cs0 = await expCurves.curveScaling(
       inputs[0],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
       curvature,
       true,
     );
     console.log(cs0.toString());
     const cs1 = await expCurves.curveScaling(
       inputs[1],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
       curvature,
       true,
     );
     console.log(cs1.toString());
     const cs2 = await expCurves.curveScaling(
       inputs[2],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
       curvature,
       true,
     );
@@ -164,28 +190,28 @@ describe("Exponential Curve", async function () {
   it("Should get Time Elapsed Ratio", async function () {
     const ter0 = await expCurves.timeElapsedRatio(
       inputs[0],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
     );
     console.log(ter0.toString());
     const ter1 = await expCurves.timeElapsedRatio(
       inputs[1],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
     );
     console.log(ter1.toString());
     const ter2 = await expCurves.timeElapsedRatio(
       inputs[2],
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
     );
     console.log(ter2.toString());
   });
 
   it("Should get Total Time Intervals", async function () {
     const tti0 = await expCurves.totalTimeInterval(
-      finalTimeframe,
       initialTimeframe,
+      finalTimeframe,
     );
     console.log(tti0.toString());
   });
