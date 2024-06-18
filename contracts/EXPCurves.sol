f72398d480e45aa07d33c20d9346715da73860ae
0xneves <asguilift@hotmail.com>
2024-06-19 05:37:01 +0700

    feat: interface for expcurve

--- contracts/EXPCurves.sol
+++ contracts/EXPCurves.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {IEXPCurves} from "./IEXPCurves.sol";
 import {exp} from "@prb/math/src/sd59x18/Math.sol";
 import {wrap, unwrap} from "@prb/math/src/sd59x18/Casting.sol";
 
@@ -13,7 +14,7 @@ import {wrap, unwrap} from "@prb/math/src/sd59x18/Casting.sol";
  * curve formula that allows for smooth, nonlinear transitions over time, providing a more
  * sophisticated and flexible approach compared to linear models.
  */
-abstract contract EXPCurves {
+abstract contract EXPCurves is IEXPCurves {
   /**
    * @notice The initial timeframe is invalid.
    *
@@ -38,43 +39,7 @@ abstract contract EXPCurves {
   error EXPCurveInvalidCurvature();
 
   /**
-   * @dev This function calculates the exponential decay value over time.
-   *
-   * This formula ensures that the value starts at 100%/0% at the beginning (t0)
-   * and decreases/increases to 0%/100% at the end (T), following an exponential decay curve.
-   *
-   * The formula used for the curves difers based on the `ascending` parameter:
-   *
-   * ascending = ((exp(k * (1 - (t - t0) / (T - t0))) - 1) / (exp(k) - 1)) * 100
-   * descenging = ((exp(k * ((t - t0) / (T - t0))) - 1) / (exp(k) - 1)) * 100
-   *
-   * Where:
-   * - t is the current timestamp
-   * - t0 is the start timestamp
-   * - T is the end timestamp
-   * - k is the curvature factor, determining the steepness of the curve (2 decimals precision)
-   * - exp() is the exponential function with base 'E' (Euler's number, approximately 2.71828)
-   *
-   * Requirements:
-   *
-   * - The initial timestamp must be less than or equal to the current timestamp
-   * - The initial timestamp must be less than the final timestamp
-   * - The curvature cannot be zero
-   * - The curvature cannot be bigger than 10000 or smaller than -10000 (2 decimals precision)
-   *
-   * NOTE: To avoid precision issues, the formula uses fixed-point math with 18 decimals.
-   * When returning this function result, make sure to adjust the output values accordingly.
-   *
-   * NOTE: Using type uint32 for timestamps since 4294967295 unix seconds will only overflow
-   * in the year 2106, which is more than enough for the current use cases.
-   *
-   * @param currentTimeframe The current timestamp or a point within the spectrum
-   * @param initialTimeframe The initial timestamp or the beginning of the curve
-   * @param finalTimeframe The final timestamp or the end of the curve
-   * @param curvature The curvature factor. Determines the steepness of the curve and can be
-   * negative, which will invert the curve's direction.
-   * @param ascending The curve direction (ascending or descending)
-   * @return int256 The exponential decay value at a specific interval
+   * @dev See {IEXPCurves-expcurve}.
    */
   function expcurve(
     uint32 currentTimeframe,
